9a35817d6f48d5afc9a43e015dc4fd5fbfe53536
Allen Robel <arobel@cisco.com>
2025-07-02 18:02:45 -1000

    Initial Commit

---
+++ NdVRF/NdVRFApp.swift
@@ -0,0 +1,17 @@
+//
+//  NdVRFApp.swift
+//  NdVRF
+//
+//  Created by Allen Robel on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NdVRFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NdVRFTests/NdVRFTests.swift
@@ -0,0 +1,17 @@
+//
+//  NdVRFTests.swift
+//  NdVRFTests
+//
+//  Created by Allen Robel on 7/2/25.
+//
+
+import Testing
+@testable import NdVRF
+
+struct NdVRFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
